9765eda2b4717e0bee2c65a8d04845ab97b2a82e
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-08-06 08:27:54 +0200

    docs: about the repo

--- welcome.py
+++ welcome.py
@@ -5,8 +5,9 @@ using the bazel-stack-vscode Visual Studio Code extension,
 quite good editor support is available for the starlark language.
 """
 
-def hello(name = "World"):
+
+def hello(name="World"):
     """
     The hello function is an example function implemented in starlark.
     """
-    print("Hello, %s!" % (name))  # buildifier: disable=print
+    print("Hello, %s!" % (name))
